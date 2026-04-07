f9a01dcf976f717bbab2bb4c76eaad65d6e2b421
Hutej <hutejmane880@gmail.com>
2026-04-07 15:01:07 +0530

    removed unused imports

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import tensorflow as tf
 import pickle
 
